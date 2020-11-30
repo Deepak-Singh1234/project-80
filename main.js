223fe7314ac407d4f9fe2bb3509798989a7087c8
Deepak-Singh1234 <71645902+Deepak-Singh1234@users.noreply.github.com>
2020-11-30 17:27:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function showparagraph1(){
+    var inputs=[];
+}
+for(var i=1;i=6;i++){
+    inputs.push(document.getElementById("p1" + i).value)
+}
+inputs.join(".");
+document.getElementById("showparagraph1").innerHTML =inputs.join(".");
+function showparagraph2(){
+    var inputs=[];
+}
+for(var i=1;i=6;i++){
+    inputs.push(document.getElementById("p1" + i).value)
+}
+inputs.join(".");
+document.getElementById("showparagraph2").innerHTML =inputs.join(".");
